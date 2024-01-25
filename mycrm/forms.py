acdc00eec9f949aa76845ea4009b329ee514c042
morelucks <luckykamshak@gmail.com>
2024-01-25 05:02:59 +0000

    template modifn

--- mycrm/forms.py
+++ mycrm/forms.py
@@ -4,10 +4,10 @@ from django.contrib.auth.forms import UserChangeForm
 
 class SignUpForm(UserChangeForm):
     name=forms.CharField(label='Name', widget=forms.TextInput(attrs={'class':'form-control'}))
-    email=forms.EmailField(label='Email Adress', widget=forms.TextInput(attrs={'class':'form-control'}))
+    email=forms.EmailField(label='Email Adress', widget=forms.EmailInput(attrs={'class':'form-control'}))
     username=forms.CharField(label='Username', widget=forms.TextInput(attrs={'class':'form-control'}))
-    password1=forms.CharField(label='Password', widget=forms.TextInput(attrs={'class':'form-control'}))
-    password2=forms.CharField(label='Confirm Password', widget=forms.TextInput(attrs={'class':'form-control'}))
+    password1=forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2=forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
     class Meta:
         model=User
